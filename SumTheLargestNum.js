5ba9b5047611ea00288cc8eb37b6a3b496bbf34e
Tome94 <tommy_yang621@hotmail.com>
2022-02-21 06:19:15 +0000

    completed adding largest and second largest number

--- SumTheLargestNum.js
+++ SumTheLargestNum.js
@@ -1,8 +1,11 @@
+/*
 const sumTheLargestNumbers = function (data){
 let largestNum = 0
 let indexOfLargest = 0
 let secondLargest = 0
 for (let i =0; i < data.length; i++){
+  //data.forEach (function(value,index){//})
+  //data.forEach ((value, index)=>{if statment})
   if (data[i]>largestNum) {
     largestNum = data[i];
     indexOfLargest = i
@@ -22,3 +25,28 @@ return largestNum+secondLargest
 console.log (sumTheLargestNumbers([1,10]));
 console.log (sumTheLargestNumbers([1,2,3]));
 console.log (sumTheLargestNumbers ([10, 4, 34, 6, 92, 2]));
+*/
+const sumTheLargestNumbers = function (data){
+  let largestNum = 0
+  let indexOfLargest = 0
+  let secondLargest = 0
+  for (const [index, value] of data.entries()){
+    if (value>largestNum) {
+      largestNum = value;
+      indexOfLargest = index
+      }
+    }
+  data.splice (indexOfLargest, 1)
+  for (const value of data){
+    if (value>secondLargest) {
+      secondLargest = value;
+      }
+    }
+  //console.log (largestNum)
+  //console.log (indexOfLargest)
+  return largestNum+secondLargest
+  
+  }
+  console.log (sumTheLargestNumbers([1,10]));
+  console.log (sumTheLargestNumbers([1,2,3]));
+  console.log (sumTheLargestNumbers ([10, 4, 34, 6, 92, 2]));
