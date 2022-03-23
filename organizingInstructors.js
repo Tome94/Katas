33f0e2d257e08ceeaab5c1939c8ff7b1b5bacd47
Tome94 <tommy_yang621@hotmail.com>
2022-03-23 19:24:42 +0000

    turning an array of objects into a single object with arrays

---
+++ organizingInstructors.js
@@ -0,0 +1,26 @@
+const organizeInstructors = function(instructors) {
+  let output = {};
+  for (const instructor of instructors) {
+
+    if (output[instructor.course]) {
+      output[instructor.course].push(instructor.name);
+    } else {
+      output[instructor.course] = [instructor.name];
+    }
+    console.log(Array.isArray(output[instructor.course]));
+  }
+  return output;
+};
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
